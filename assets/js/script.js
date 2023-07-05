9fabdee8ac1d6f86cfebd2179b304f861801a914
Aclambert95 <aclambert95@outlook.com>
2023-07-04 19:51:48 -0500

    added the function generate the password in the text area

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,3 +11,9 @@ var randPassword = ""
 //Used for "Generate Password" button
 generateBtn.addEventListener("click", writePassword);
 
+// Write password to the #password input
+function writePassword() {
+    var password = generatePassword();
+    var passwordText = document.querySelector("#password");
+    passwordText.value = password;
+  }
